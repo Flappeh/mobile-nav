751eebe59a34985a5c7407fc2d9dffa8b7670263
flappeh <davidbonar772@gmail.com>
2024-05-15 01:38:54 +0700

    finished scaffolding project structure, need to add components and handle data passing

---
+++ components/CustomButton.tsx
@@ -0,0 +1,27 @@
+import { View, TouchableOpacity,Text } from 'react-native'
+import React from 'react'
+
+interface buttonProps{
+    title: string,
+    handlePress?: ()=> {} | undefined | null,
+    containerStyles?: string,
+    textStyles?: string,
+    isLoading?: boolean
+}
+
+const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}: buttonProps) => {
+  return (
+    <TouchableOpacity 
+        className={`bg-secondary-100 justify-center items-center min-h-[62px] rounded-xl ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+        onPress={handlePress}
+        activeOpacity={0.7}
+        disabled={isLoading}
+        >
+        <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+            {title}
+        </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
